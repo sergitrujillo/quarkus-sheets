22eded3ab7fc7483e4073d8bc3e2a16f3edbf58c
Sergi Trujillo <sergi.trujillo@gmail.com>
2020-01-16 21:42:37 +0100

    Duplicate project in two folders part 1 and part 3 in order to maintain full version of each one and no need to navigate between tags or commits

---
+++ part3/src/main/java/cat/udl/PersonService.java
@@ -0,0 +1,58 @@
+package cat.udl;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.util.Arrays;
+import java.util.List;
+
+@Path("/persons")
+public class PersonService {
+
+
+    @Inject
+    PersonsRepository personsRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Person> getAll() {
+        return personsRepository.getAll();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person get(@PathParam("id") int id) {
+        return personsRepository.get(id)
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(Person person, @Context UriInfo uriInfo) {
+        int id = personsRepository.add(person);
+        UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+        builder.path(Integer.toString(id));
+        return Response.created(builder.build()).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person replace(@PathParam("id") int id, Person person) {
+        return personsRepository.replace(id, person)
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person remove(@PathParam("id") int id) {
+        return personsRepository.remove(id)
+                .orElseThrow(NotFoundException::new);
+    }
+
+
+}
---
+++ part3/src/test/java/cat/udl/NativeExampleIT.java
@@ -0,0 +1,9 @@
+package cat.udl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleIT extends ExampleTest {
+
+    // Execute the same tests but in native mode.
+}
