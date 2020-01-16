b26d54851a50d4d8a03f8bc4704cb2dd9ef14c95
Sergi Trujillo <sergi.trujillo@gmail.com>
2020-01-16 20:23:56 +0100

    Get list and get one

--- src/main/java/cat/udl/PersonService.java
+++ src/main/java/cat/udl/PersonService.java
@@ -1,17 +1,31 @@
 package cat.udl;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/persons")
 public class PersonService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Person getPerson() {
-        Person person = new Person("Obi-Wan", "Kenobi");
-        return person;
+    public List<Person> getAll() {
+        Person person1 = new Person("Obi-Wan", "Kenobi");
+        Person person2 = new Person("Leia", "Organa");
+        return Arrays.asList(person1,person2);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Person hello(@PathParam("id") int id) {
+        Person person1 = new Person("Obi-Wan", "Kenobi");
+        Person person2 = new Person("Leia", "Organa");
+        List<Person> people = Arrays.asList(person1, person2);
+        if (id>=0 && id<people.size()){
+            return people.get(id);
+        }
+        throw new NotFoundException(); // to return 404
     }
 }
