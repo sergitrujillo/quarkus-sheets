f3935bfec371a1939a3acbbf66297f84fad46f86
Sergi Trujillo <sergi.trujillo@gmail.com>
2020-01-16 19:54:47 +0100

    Project base created with maven command

---
+++ src/main/java/cat/udl/Example.java
@@ -0,0 +1,16 @@
+package cat.udl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Example {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
